91e4ba0d7b3f6310f6f195eba56b10a07a26a5d8
berbaroovez <carterolson3@gmail.com>
2021-02-17 02:12:27 -0600

    nighly build test

--- components/PickUpTable.tsx
+++ components/PickUpTable.tsx
@@ -1,9 +1,10 @@
 // type PickUpProps = {
 import React, { FunctionComponent, useState, useEffect } from "react";
 
-import { format } from "date-fns";
+import { format, parseISO, isSameDay } from "date-fns";
 import { Order } from "@/utils/interfaces";
 import { Link, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { da } from "date-fns/locale";
 
 // }
 
@@ -16,8 +17,10 @@ export const PickUpTable: FunctionComponent<TableProps> = ({
   startDate,
 }) => {
   const [calender, setCalender] = useState<Array<Date>>(undefined);
-
+  const [calenderOrders, setCalenderOrders] = useState<Array<Order>>(undefined);
+  const [counter, setCounter] = useState<number>(0);
   useEffect(() => {
+    setCalenderOrders(orders);
     let tempCalender: Array<Date> = [];
 
     for (let i = 0; i <= 5; i++) {
@@ -28,15 +31,21 @@ export const PickUpTable: FunctionComponent<TableProps> = ({
     setCalender(tempCalender);
   }, []);
 
-  // for (let i = 0; i <= 5; i++) {
-  //   let currentDate: Date = new Date();
-  //   currentDate.setDate(startDate.getDate() + i);
-  //   calender.push(format(currentDate, "EEEE do"));
-  // }
-  // <Td>{format(parseISO(order.eventDate), "P")}</Td>;
-
+  const pickupsForDate = (pickupDay: Date) => {
+    console.log("DATE________________________", pickupDay);
+    let pickupOrderArray: Array<Order> = [];
+    for (let i = counter; i < calenderOrders.length; i++) {
+      console.log("Calender", calenderOrders[i].pickupDate);
+      if (isSameDay(parseISO(calenderOrders[i].pickupDate), pickupDay)) {
+        pickupOrderArray.push(calenderOrders[i]);
+        setCounter(counter + 1);
+      }
+    }
+    console.log(pickupOrderArray);
+    return pickupOrderArray;
+  };
   return (
-    <Table background="white" borderRadius="20px" height={16}>
+    <Table background="white" borderRadius="20px" variant="unstyled">
       <Thead>
         <Th>Name</Th>
         <Th>Pick Up Time</Th>
@@ -46,9 +55,22 @@ export const PickUpTable: FunctionComponent<TableProps> = ({
       <Tbody>
         {calender ? (
           calender.map((day) => (
-            <Tr>
-              <Td>{format(day, "EEEE do")}</Td>
-            </Tr>
+            <>
+              <Tr>
+                <Td
+                  fontWeight="bold"
+                  color="teal"
+                  borderBottom="1px teal solid"
+                >
+                  {format(day, "EEEE do")}
+                </Td>
+              </Tr>
+              {pickupsForDate(day).map((pickup) => {
+                <Tr>
+                  <Td>{pickup.name}</Td>
+                </Tr>;
+              })}
+            </>
           ))
         ) : (
           <Tr>
@@ -65,3 +87,7 @@ export const PickUpTable: FunctionComponent<TableProps> = ({
 //         ) : (
 //           <Td> "NAY"</Td>
 //         )}
+
+// {
+//   isSameDay(parseISO(calenderOrders[2].pickupDate), day) ? "Yep" : "Nope";
+// }
